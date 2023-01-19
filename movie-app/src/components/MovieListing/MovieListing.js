d425a9f4f26efa55d05884251f93eb06050d048a
sajeewa garusinghe <sajeeva616@gmail.com>
2023-01-19 14:10:57 +0530

    movie list display fine

--- movie-app/src/components/MovieListing/MovieListing.js
+++ movie-app/src/components/MovieListing/MovieListing.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { getAllMovies } from '../../features/movies/movieSlice';
+import MovieCard from '../MovieCard/MovieCard';
+import './MovieListing.scss';
 
 const MovieListing = () => {
   const movies = useSelector(getAllMovies);
-  console.log("MovieListing");
-  console.log(movies);
-  return <div>MovieListing</div>;
+  let renderMovies = '';
+  // console.log(movies.Response);
+  renderMovies =
+    movies.Response === 'True' ? (
+      movies.Search.map(
+        (movie, index) => <MovieCard key={index} data={movie} />
+        // console.log(movie);
+      )
+    ) : (
+      <div className="movies-error">
+        <h3>{movies.Error}</h3>
+      </div>
+    );
+  // console.log('MovieListing');
+  // console.log(movies);
+  return (
+    <div className="movie-wrapper">
+      <div className="movie-list">
+        <h2>Movies</h2>
+        <div className="movie-container">{renderMovies}</div>
+      </div>
+    </div>
+  );
 };
 
 export default MovieListing;
